9b4af012f2894213abced89da5fd09f51094f3c7
zzz47zzz <52692494+zzz47zzz@users.noreply.github.com>
2019-10-13 18:50:38 +0800

    django后台初始代码
    连接的是本地mysql

---
+++ mydb/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class teacher(models.Model):
+    id = models.AutoField
+    teacherId = models.CharField(null=False,max_length=80)
+    weChatId = models.CharField(null=False,max_length=80)
+    name = models.CharField(null=False,default="NoUserNick",max_length=255)
+    class Meta:
+        #managed = False
+        db_table = 'teacher'
+
+
+class rooms(models.Model):
+    id = models.AutoField
+    roomId = models.CharField(null=False,max_length=80)
+    maxCap = models.IntegerField(null=False,default=0)
+    roomType = models.IntegerField(null=False,default=0)
+    class Meta:
+        #managed = False
+        db_table = 'rooms'
+
+class rooms_teacher(models.Model):
+    id = models.AutoField
+    roomId = models.CharField(null=False,max_length=80)
+    teacherId = models.CharField(null=False,max_length=80)
+    date =  models.CharField(null=False,max_length=80)
+    time =  models.CharField(null=False,max_length=80)
+    class Meta:
+        #managed = False
+        db_table = 'rooms_teacher'
