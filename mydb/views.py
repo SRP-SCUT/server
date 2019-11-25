8456ed15307f9f348aae35ff2c11b8401849fcea
WZW <1019510954@qq.com>
2019-11-25 11:20:49 +0800

    修改views.py部分，query结果为空用len(query_result)==0表示，同时query出来的item是object对象

--- mydb/views.py
+++ mydb/views.py
@@ -10,36 +10,47 @@ def get_history(request):
     nickName=data['nickName']
     workNum=data['workNum']
     all_information=models.rooms_teacher.objects.filter(teacherId=workNum)
-    result={"code":1,"msg":"查询失败","data":[]}
-    if all_information==None:
-        result['code']=1
+    result={"code":0,"msg":"查询失败","data":[]}#0表示失败，1表示成功
+    '''if all_information==None:
+        result['code']=0
+        result['msg']="查询失败"'''
+    if len(all_information)==0:
+        result['code']=0#0表示失败
         result['msg']="查询失败"
     else:
-        result['code']=0
+        result['code']=1#1表示成功
         result['msg']="查询成功"
         i=0
         for item in all_information:
             if i>=15:
                 break
             i+=1
-            result["data"].append({"room":item['roomId'],"roomType":item['roomType'],"date":item['date'],
-                                   "timeslot":item['time'],"status":item['status']})
+            '''result["data"].append({"room":item['roomId'],"roomType":item['roomType'],"date":item['date'],
+                                   "timeslot":item['time'],"status":item['status']})'''
+            result["data"].append({"room":item.roomId,"roomType":item.roomType,"date":item.date,
+                                   "timeslot":item.time,"status":item.status})
     Jsondata=json.dumps(result)
     #Jsondata是返回的Json数据
     return HttpResponse(Jsondata,content_type='application/json')
 #前端还是用post方法！
 def get_appointment(request):
     all_information = models.rooms_teacher.objects.filter(status=2)#2代表待处理
-    result = {"code": 1, "msg": "查询失败", "data": []}
-    if all_information==None:
-        result['code'] = 1
-        result['msg'] = "查询失败"
+    result = {"code": 0, "msg": "查询失败", "data": []}#0表示失败，1表示成功
+    '''if all_information==None:
+        result['code'] = 0
+        result['msg'] = "查询失败"'''
+    if len(all_information)==0:
+        result['code']=0#0表示失败
+        result['msg']="查询失败"
     else:
-        result['code']=0
+        result['code']=1
         result['msg']="查询成功"
         for item in all_information:
-            result["data"].append({"workNum":item['teacherId'],"teacherName":item['name'],"roomType":item['roomType'],
-                                   "roomId":item['roomId'],"date":item['date'],"timeslot":item['time']})
+            '''result["data"].append({"workNum":item['teacherId'],"teacherName":item['name'],"roomType":item['roomType'],
+                                   "roomId":item['roomId'],"date":item['date'],"timeslot":item['time']})'''
+            result["data"].append(
+                {"workNum": item.teacherId, "teacherName": item.name, "roomType": item.roomType,
+                 "roomId": item.roomId, "date": item.date, "timeslot": item.time})
     Jsondata=json.dumps(result)
     #Jsondata是返回的Json数据
     return HttpResponse(Jsondata,content_type='application/json')
